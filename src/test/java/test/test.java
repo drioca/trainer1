a5a7132ab2aafb183d31e9f602fea0820862660f
Peter Besze <pbesze@gmail.com>
2016-09-06 09:26:12 +0200

    Create test.java

---
+++ src/test/java/test/test.java
@@ -0,0 +1,14 @@
+//import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+/*
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+}
+*/
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms 
